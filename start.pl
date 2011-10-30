83faecca60422b2fc73873745799c79df8338df3
Brent <betz_b@Brent-Betzs-MacBook-Pro.local>
2011-10-30 12:33:11 -0400

    Initial Commit
    Initial commit...

---
+++ start.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/env perl
+
+use strict;
+
+use warnings;
+
+# *** Testing GitHub for Mac...
+
+# -------------------------------------------
+
+print "Dart does not stand a chance.";
+
+# -------------------------------------------
